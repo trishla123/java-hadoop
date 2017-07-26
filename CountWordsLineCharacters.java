3d7e3c72801da13ec9c47b5cbbde55e906eefeee
trishla123 <trish.thakur@gmail.com>
2017-07-26 23:19:56 +0530

    countWordsLineCharactersLatestVersion
    if possible pls give me inputs which are things in problem statement...

--- CountWordsLineCharacters.java
+++ CountWordsLineCharacters.java
@@ -4,104 +4,92 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 
 
-public class CountWordsLineCharacters 
+public class CountWordsLineCharacters {
 
-{
-
-    public static void readFileAndPrintCounts(String File) throws FileNotFoundException
- {
+    public static void readFileAndPrintCounts(String File) throws FileNotFoundException {
 
         int TotalWords = 0;
         int TotalLines = 0;
 
         String Line;
-	Date st = new Date();
 	
-       
-      try( BufferedReader Buffer = new BufferedReader(new FileReader(File)))
+	
+ try( BufferedReader Buffer = new BufferedReader(new FileReader(File)))
 
+      {
+            
 
-        {
             Map<String, Integer> mapOfRepeatedWord = new HashMap<String, Integer>();
 
-                  
+ 
 
-                 while (( Line = Buffer.readLine()) != null) 
+while (( Line = Buffer.readLine()) != null) 
 
             {
-                Log(Line);
+		
+               
                 TotalLines++;
 
 
                 String[] myWords = Line.replaceAll("\\s+", " ").split(" ");
 
                 for (String s : myWords) 
-             
-                 {
+    {
                     
+
                     String tempUCword = s.toLowerCase();
 
 
-                    if (mapOfRepeatedWord.containsKey(tempUCword)) 
- 
-                   {
+                    if (mapOfRepeatedWord.containsKey(tempUCword))
+                 {
 
+                      
                         mapOfRepeatedWord.put(tempUCword, mapOfRepeatedWord.get(tempUCword) + 1);
 
                     } 
-        
-                 else 
- 
-                    {
+                   else 
+                         {
+                             mapOfRepeatedWord.put(tempUCword, 1);
+                         }
+      }
+}
 
-                      
-                  mapOfRepeatedWord.put(tempUCword, 1);
-                    
-		
-	       
+
+               
                 System.out.println("Count of the words : \n");
-                System.out.println("Words" + "\t\t" + "Count");
-                System.out.println("======" + "\t\t" + "=====");
+                System.out.println("Words" + "\t\t\t" + "Count");
+                System.out.println("======" + "\t\t\t" + "=====");
 
 
                 for (Map.Entry<String, Integer> entry : mapOfRepeatedWord.entrySet())
-          
-              {
-                    System.out.println(entry.getKey() + "\t\t" + entry.getValue());
-              }
+    
+      {
+                    System.out.println(entry.getKey() + "\t\t\t" + entry.getValue());
+      }
+
+}
+
         
 
-              catch(IOException e)
+        catch(IOException e)
                 {
                     e.printStackTrace();
                 }
 
-            }
+      }
+        private static void Log(String string)
+    {
+        System.out.println(string);
+    }
 
-
-            private static void Log(String string) 
-
-                      {
-                              System.out.println(string);
-                      }
-
-            public static void main(String[] args) 
-
-  {
-
-       try 
-        
-        {
-            String File = "C:\Users\Admin\text.txt";
+    public static void main(String[] args) {
+        try {
+            String File = "/C:/Users/609753284/test.txt";
             readFileAndPrintCounts(File);
         }
 
@@ -109,7 +97,5 @@ public class CountWordsLineCharacters
         {
             e.printStackTrace();
         }
-    
     }//main
-
 }//class
